0610a703372f8fa7e37f5517d32eecf0a0fb89bd
Maggie Flatt <maggieflatt19@gmail.com>
2026-05-04 10:21:55 -0500

    Initial commit

---
+++ WanderLensapi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WanderlensapiConfig(AppConfig):
+    name = 'WanderLensapi'
---
+++ seed_database.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm db.sqlite3
+rm -rf ./WanderLensapi/migrations
+python3 manage.py migrate
+python3 manage.py makemigrations WanderLensapi
+python3 manage.py migrate WanderLensapi
+python3 manage.py loaddata users
+python3 manage.py loaddata tokens
+

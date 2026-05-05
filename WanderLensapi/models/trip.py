ecdbb11e7c67f6e25edf76f6909f07762a514f4b
Maggie Flatt <maggieflatt19@gmail.com>
2026-05-04 20:44:57 -0500

    added trip model

---
+++ WanderLensapi/models/trip.py
@@ -0,0 +1,23 @@
+"""User Trip model"""
+
+from django.conf import settings
+from django.db import models
+from .triptype import TripType
+
+
+class Trip(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="trips",
+    )
+    trip_type = models.ForeignKey(
+        TripType,
+        on_delete=models.PROTECT,
+        related_name="trips",
+    )
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+    color = models.CharField(max_length=7, default="#3B82F6")
+    start_date = models.DateField()
+    is_private = models.BooleanField(default=False)

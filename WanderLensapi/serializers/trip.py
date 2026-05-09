66f745e7b1e42a69aab5746351b95e17ca872342
Maggie Flatt <maggieflatt19@gmail.com>
2026-05-09 10:25:16 -0500

    added stopsto trip serializer

--- WanderLensapi/serializers/trip.py
+++ WanderLensapi/serializers/trip.py
@@ -1,9 +1,13 @@
 from WanderLensapi.models import Trip
 from rest_framework import serializers
 
+from WanderLensapi.serializers.stop import StopSerializer
+
 
 class TripSerializer(serializers.ModelSerializer):
     """JSON serializer for Trip"""
+
+    stops = StopSerializer(many=True, read_only=True, source="stop_set")
 
     class Meta:
         model = Trip
@@ -16,5 +20,6 @@ class TripSerializer(serializers.ModelSerializer):
             "start_date",
             "is_private",
             "color",
+            "stops",
         )
         depth = 1

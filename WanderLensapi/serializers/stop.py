9f95080b59d3cdd7ea79a524ac61001591950cb6
Maggie Flatt <maggieflatt19@gmail.com>
2026-05-09 10:24:32 -0500

    added category serializer to stop serializer

--- WanderLensapi/serializers/stop.py
+++ WanderLensapi/serializers/stop.py
@@ -1,21 +1,25 @@
 from WanderLensapi.models import Stop
 from rest_framework import serializers
 
+from WanderLensapi.serializers.category import CategorySerializer
+
 
 class StopSerializer(serializers.ModelSerializer):
     """JSON serializer for Stop"""
 
+    categories = CategorySerializer(many=True, read_only=True)
+
     class Meta:
         model = Stop
         fields = (
-            "id", 
-            "trip", 
-            "name", 
-            "description", 
-            "city", 
+            "id",
+            "trip_id",
+            "name",
+            "description",
+            "city",
             "country",
             "latitude",
             "longitude",
             "visited_date",
+            "categories",
         )
-        depth = 1

002b9518c570e05f907ec7d987a09b3b1d06cfe5
Maggie Flatt <maggieflatt19@gmail.com>
2026-05-12 11:18:43 -0500

    fixed bug where message on line 92 and 95 were missing quotes

--- WanderLensapi/views/trip.py
+++ WanderLensapi/views/trip.py
@@ -89,10 +89,10 @@ class TripView(ViewSet):
             return Response(None, status=status.HTTP_204_NO_CONTENT)
 
         except Trip.DoesNotExist as ex:
-            return Response({message: ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"message": ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
 
         except Exception as ex:
-            return Response({message: ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response({"message": ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def list(self, request):
         """Handle GET requests for all items

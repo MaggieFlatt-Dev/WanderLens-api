9b937b0f4f632f566d43932bb806ff83ef336374
Maggie Flatt <maggieflatt19@gmail.com>
2026-05-15 10:08:37 -0500

    added null=true and blank=true to handle api returning locations with no city

--- WanderLensapi/models/stop.py
+++ WanderLensapi/models/stop.py
@@ -5,7 +5,7 @@ class Stop(models.Model):
     trip = models.ForeignKey("Trip", on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=255)
-    city = models.CharField(max_length=50)
+    city = models.CharField(max_length=50, null=True, blank=True)
     country = models.CharField(max_length=50)
     latitude = models.FloatField()
     longitude = models.FloatField()

782002c2636bcf998877b92e3029c58813b59ecc
Maggie Flatt <maggieflatt19@gmail.com>
2026-05-26 15:13:42 -0500

    added static for photos

--- WanderLensproject/urls.py
+++ WanderLensproject/urls.py
@@ -1,5 +1,7 @@
 
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static 
 from django.urls import include, path
 from rest_framework import routers
 from WanderLensapi.views import (
@@ -26,5 +28,5 @@ urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('current_user', get_current_user)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
